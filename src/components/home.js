8c5f51a61ff72606c75d1636e8ab0a32b1b60dd8
mia16zang <manya.mankad@gmail.com>
2023-05-12 14:53:19 +0530

    Your commit message here

--- src/components/home.js
+++ src/components/home.js
@@ -14,15 +14,18 @@ function Home() {
       <div>
         <Navbar />
       </div>
-      <div className="flex align-center pt-40 px-40 z-10">
+      <div className="bg-white flex align-center pt-40 px-40 z-10 py-20">
         <h1 className="text-5xl font-bold flex justify-center">
           Welcome! This is my project on visualizing Climate Change using Fun
           Theory.
         </h1>
       </div>
-      <div>
+      <div className="container p-4 grid grid-cols-4">
+        <h2 className="text-2xl font-medium col-start-2 my-5">
+          Go to quiz here:
+        </h2>
         <button
-          className="mt-4 bg-blue-500 text-white px-4 py-2 rounded-md"
+          className="flex justify-center col-start-3 mt-4 bg-white shadow-lg hover:bg-gray-100 font-medium items-center text-black px-4 py-2 rounded-md"
           onClick={handleSubmit}
         >
           Start
