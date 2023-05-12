4d92aee7c1ea79166ac61c4a172080b0d4c50ff6
mia16zang <manya.mankad@gmail.com>
2023-05-12 15:44:13 +0530

    commit final

--- src/components/chart4.js
+++ src/components/chart4.js
@@ -1,15 +1,12 @@
 import React from "react";
 
 function Area() {
-  const chartUrl =
-    "https://public.flourish.studio/visualisation/13728373//embed?auto=1&header=0&footer=0";
-
   return (
     <div style={{ height: "800px" }}>
       <iframe
         title="Chart 4"
         className="w-full h-full"
-        src={chartUrl}
+        src="https://public.flourish.studio/visualisation/13734088/embed?header=0&footer=0"
         allowFullscreen
         frameBorder="0"
       ></iframe>
